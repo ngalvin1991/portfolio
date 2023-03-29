da6287c941754f524ef0ff70f19034b1d8256d17
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-03-29 18:09:35 +0100

    components added - app.js including component links, about me section complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import AboutMe from './Components/AboutMe';
+import ContactMe from './Components/ContactMe';
+import Navbar from './Components/Navbar';
+import ProjectSection from './Components/ProjectSection';
+import Skills from './Components/Skills';
+import Footer from './Components/Footer';
 
-function App() {
+
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='text-black-400 bg-white body-font'>
+    <Navbar />
+    <AboutMe />
+    <ProjectSection />
+    <Skills />
+    <ContactMe />
+    <Footer />
+    </main>
   );
 }
-
-export default App;
---
+++ src/Components/AboutMe.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function AboutMe () {
+    return (
+       <section id="about">
+        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left
+        mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                Hey there, I'm Noel!
+                <br className="hidden lg:inline-block" /> Front-End Web Developer. 
+            </h1>
+            <p className="mb-8 leading-relaxed">
+                I'm Noel, a Front-End Web Developer from South London. I have a great passion
+                for creating immersive and visually pleasing applications. 
+                I am very well organised, a great problem solver and have a very high attention to detail. 
+
+                Besides developing websites, I'm a very big fan of music, films and American Football. 
+
+                I'm very interested in all things front-end and want to work on ambitious projects with amazing people.
+            </p>
+            <div className=" flex justify-center">
+                <a 
+                href="#contact"
+                className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                    Work With Me!
+                </a>
+                <a
+                href="#projects"
+                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                    My Projects
+                </a>
+            </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        </div>
+        </div>
+        </section>
+    )
+}
